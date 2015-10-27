0f32af423627c10f7c6080b972835335728da19c
eXiga <anton.kostenich@hotmail.com>
2015-10-27 22:35:43 +0300

    Initial commit

---
+++ STV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STV
+//
+//  Created by Anton Kostenich on 10/27/15.
+//  Copyright © 2015 Anton Kostenich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ STV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  STV
+//
+//  Created by Anton Kostenich on 10/27/15.
+//  Copyright © 2015 Anton Kostenich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
