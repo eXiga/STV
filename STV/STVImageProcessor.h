82a31b56e681e5984d6d493997370306ca14c72f
eXiga <anton.kostenich@hotmail.com>
2015-10-27 22:56:39 +0300

    Add simple app backbone

---
+++ STV/STVImageProcessor.h
@@ -0,0 +1,13 @@
+//
+//  STVImageProcessor.h
+//  STV
+//
+//  Created by Anton Kostenich on 10/27/15.
+//  Copyright © 2015 Anton Kostenich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STVImageProcessor : NSObject
+
+@end
---
+++ STV/STVImageProcessor.m
@@ -0,0 +1,13 @@
+//
+//  STVImageProcessor.m
+//  STV
+//
+//  Created by Anton Kostenich on 10/27/15.
+//  Copyright © 2015 Anton Kostenich. All rights reserved.
+//
+
+#import "STVImageProcessor.h"
+
+@implementation STVImageProcessor
+
+@end
